d1ad57f7e74f1d51a836e2680b2aaca82dbb3ebc
lawrencedulian <lawrenceedulian@gmail.com>
2023-02-13 17:03:26 +0100

    Updated ui card

--- src/components/CardApartment.vue
+++ src/components/CardApartment.vue
@@ -14,9 +14,9 @@ export default {
     descriptionPreview() {
       if (
         this.apartment.description &&
-        this.apartment.description.length > 100
+        this.apartment.description.length > 80
       ) {
-        return this.apartment.description.substr(0, 100) + " ...";
+        return this.apartment.description.substr(0, 80) + " ...";
       }
       return this.apartment.description;
     },
@@ -25,45 +25,57 @@ export default {
 </script>
 
 <template>
-    <div class="col-3 my-3">
-        <div class="ms-card" style="width: 18rem;">
-            <img :src="`${store.apiUrl}/storage/${apartment.cover_image}`" class="ms-card-img-top" alt="apartment.title">
-            <div class="ms-card-body p-2">
-                <!-- Title -->
-                <h5 class="card-title mt-2">{{ apartment.title }}</h5>
-                <!-- /Title -->
+  <div class="col-3 my-3">
+    <div class="ms-card" style="width: 18rem;">
+      <img :src="`${store.apiUrl}/storage/${apartment.cover_image}`" class="ms-card-img-top" alt="apartment.title">
+      <div class="ms-card-body p-2">
+        <!-- Title -->
+        <h5 class="card-title mt-2">{{ apartment.title }}</h5>
+        <!-- /Title -->
 
-                <!-- Description -->
-                <p class="ms-card-text mt-1" v-if="apartment.description">{{ descriptionPreview }}</p>
-                <p class="ms-card-text mt-1" v-else> ---- </p>
-                <!-- / Description -->
+        <!-- Description -->
+        <p class="ms-card-text mt-1" v-if="apartment.description">{{ descriptionPreview }}</p>
+        <p class="ms-card-text mt-1" v-else> ---- </p>
+        <!-- / Description -->
 
-                <router-link
-                    :to="{
-                        name: 'show',
-                        params: { slug: apartment.slug },
-                    }"
-                    class="btn btn-primary"
-                    >
-                    <i class="fa-solid fa-eye"></i>
-                </router-link>
-            </div>
-        </div>
+        <router-link :to="{
+          name: 'show',
+          params: { slug: apartment.slug },
+        }" class="btn btn-primary">
+          <i class="fa-solid fa-eye"></i>
+        </router-link>
+      </div>
     </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
+
 .ms-card {
-    background-color: var(--bgGrey);
+  height: 100%;
+  min-height: 400px;
+  background-color: var(--bgGrey);
+  border-top-right-radius: 30px;
+
+  .ms-card-body {
+    .card-title {
+      font-size: 15px;
+      font-weight: 600;
+    }
+
+    .ms-card-text {
+      font-size: 13px;
+      font-weight: 400;
+      color: var(--darkerGrey);
+    }
+  }
+
+  .ms-card-img-top {
+    width: 18rem;
+    height: 18rem;
+    object-fit: cover;
     border-top-right-radius: 30px;
-    .ms-card-body {
-        .ms-card-text {
-            color: var(--darkerGrey);
-        }
-    }
-    .ms-card-img-top {
-        border-top-right-radius: 30px;
-        border-bottom-left-radius: 30px;
-    }
+    border-bottom-left-radius: 30px;
+  }
 }
 </style>
