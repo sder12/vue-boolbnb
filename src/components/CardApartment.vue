cb486dc8d8d3c7a3b929e29204151ba5eec7721b
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-02-13 10:57:08 +0100

    Add: FilterSearch Page

--- src/components/CardApartment.vue
+++ src/components/CardApartment.vue
@@ -1,55 +1,65 @@
 <script>
-import { store } from '../store';
+import { store } from "../store";
 export default {
-    name: "CardApartment",
-    props: {
-        apartment: Object,
+  name: "CardApartment",
+  props: {
+    apartment: Object,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  computed: {
+    descriptionPreview() {
+      if (
+        this.apartment.description &&
+        this.apartment.description.length > 100
+      ) {
+        return this.apartment.description.substr(0, 100) + " ...";
+      }
+      return this.apartment.description;
     },
-    data() {
-        return {
-            store
-        }
-    },
-    computed: {
-        descriptionPreview() {
-            if (this.apartment.description && this.apartment.description.length > 100) {
-                return this.apartment.description.substr(0, 100) + ' ...';
-            }
-            return this.apartment.description;
-        }
-    },
-}
-
+  },
+};
 </script>
 
-
 <template>
-    <div class="col col-3 my-3">
-        <div class="card" style="width: 18rem;">
+  <div class="col col-3 my-3">
+    <div class="card" style="width: 18rem">
+      <img
+        :src="`${store.apiUrl}/storage/${apartment.cover_image}`"
+        class="card-img-top"
+        alt="apartment.title"
+      />
 
-            <img :src="`${store.apiUrl}/storage/${apartment.cover_image}`" class="card-img-top" alt="apartment.title">
+      <div class="card-body">
+        <!-- Title -->
+        <h5 class="card-title">{{ apartment.title }}</h5>
+        <!-- /Title -->
 
-            <div class=" card-body">
-                <!-- Title -->
-                <h5 class="card-title">{{ apartment.title }}</h5>
-                <!-- /Title -->
+        <!-- Description -->
+        <p class="card-text" v-if="apartment.description">
+          {{ descriptionPreview }}
+        </p>
 
-                <!-- Description -->
-                <p class="card-text" v-if="apartment.description">{{ descriptionPreview }}</p>
-                <p class="card-text" v-else> ---- </p>
-                <!-- / Description -->
+        <p class="card-text" v-else>----</p>
+        <!-- / Description -->
 
-                <!-- router-link :to="{name: 'single-apartment', params: {slug: apartment.slug}}"-->
-                <a href="#" class="btn btn-primary">
-                    <i class="fa-solid fa-eye"></i>
-                </a>
-                <!-- / router-link -->
-            </div>
-        </div>
+        <!-- router-link :to="{name: 'single-apartment', params: {slug: apartment.slug}}"-->
+        <router-link
+          :to="{
+            name: 'show',
+            params: { slug: apartment.slug },
+          }"
+          class="btn btn-primary"
+        >
+          <i class="fa-solid fa-eye"></i>
+        </router-link>
+        <!-- / router-link -->
+      </div>
     </div>
+  </div>
 </template>
 
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from './pages/HomePage.vue';
 import SearchPage from './pages/SearchPage.vue';
+import ApartmentPage from "./pages/ApartmentPage.vue";
 import NotFoundPage from './pages/NotFoundPage.vue';
 
 const router = createRouter({
@@ -15,6 +16,11 @@ const router = createRouter({
             path: '/search',
             name: 'search',
             component: SearchPage
+        },
+        {
+            path: '/:slug',
+            name: 'show',
+            component: ApartmentPage,
         },
 
         // LAST: 404 not found
