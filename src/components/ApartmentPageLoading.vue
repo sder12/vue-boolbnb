effa312cd8760d03183853a85756505433cb38b6
IlCarretto <carlorebuffat412@gmail.com>
2023-02-16 17:46:03 +0100

    Fix: ApartmentPageLoading

--- src/components/ApartmentPageLoading.vue
+++ src/components/ApartmentPageLoading.vue
@@ -31,6 +31,30 @@ export default {
       </div>
     </div>
   </section>
+  <section class="fake-basic-services p-5">
+    <div class="container">
+      <div v-for="index in 5" :key="index" class="fake-icon-group d-flex align-items-center mb-3">
+        <div class="fake-icon me-3"></div>
+        <div class="fake-text">
+          <div class="fake-title mb-1"></div>
+          <div class="fake-subtitle"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="fake-advanced-services p-5">
+    <div class="container">
+      <h2 class="fake-advanced-title"></h2>
+      <div class="row" v-for="index in 5" :key="index">
+        <div class="col-xs-1 col-sm-6">
+          <div class="icon-group-2 d-flex align-items-center mt-3">
+            <div class="fake-icon me-3"></div>
+            <h5 class="fake-advanced-service-title"></h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
@@ -77,6 +101,55 @@ export default {
       color: white;
       font-size: 5rem;
     }
+  }
+
+}
+
+.fake-basic-services,
+.fake-advanced-services {
+  border-top: 1px solid var(--lightGrey);
+  border-bottom: 1px solid var(--lightGrey);
+
+}
+
+.fake-icon {
+  width: 25px;
+  height: 25px;
+  background-color: var(--darkerGrey);
+  border-radius: 5px;
+}
+
+.fake-text {
+  .fake-title,
+  .fake-subtitle {
+    background-color: var(--lightGrey);
+    border-radius: 5px;
+  }
+
+  .fake-title {
+    width: 150px;
+    height: 15px;
+  }
+  
+  .fake-subtitle {
+    width: 100px;
+    height: 10px;
+  }
+}
+
+.fake-advanced-services {
+  .fake-advanced-title {
+    width: 300px;
+        height: 20px;
+        border-radius: 10px;
+        background-color: var(--lightGrey);
+  }
+  
+  .fake-advanced-service-title {
+    width: 80px;
+    height: 20px;
+    background-color: var(--lightGrey);
+    border-radius: 5px;
   }
 }
 </style>
