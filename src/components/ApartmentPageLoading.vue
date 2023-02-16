bf6931cac1ae64589656cd4a94d524653d429b4c
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-02-16 16:50:58 +0100

    Fix: tomtom map

--- src/components/ApartmentPageLoading.vue
+++ src/components/ApartmentPageLoading.vue
@@ -1,11 +1,11 @@
 <script>
 export default {
-    name: "ApartmentPageLoading"
-}
+  name: "ApartmentPageLoading",
+};
 </script>
 
 <template>
-<section id="apartment" class="container">
+  <section id="apartment" class="container">
     <div class="row">
       <div class="col col-md-6">
         <div class="bar mt-3 mb-3"></div>
@@ -14,7 +14,7 @@ export default {
           <div class="fake-text mb-2"></div>
           <div class="fake-text"></div>
         </div>
-        <div class="fake-description mt-5 text-end">
+        <div class="fake-description mt-5 text-center">
           <p class="fake-paragraph mb-2"></p>
           <p class="fake-paragraph mb-2"></p>
           <p class="fake-paragraph mb-2"></p>
@@ -23,12 +23,14 @@ export default {
         </div>
       </div>
       <div class="col col-md-5">
-        <div class="fake-img-square d-flex align-items-center justify-content-center">
-            <i class="fa-solid fa-mountain-sun"></i>
+        <div
+          class="fake-img-square d-flex align-items-center justify-content-center mt-3"
+        >
+          <i class="fa-solid fa-mountain-sun"></i>
         </div>
       </div>
     </div>
-</section>
+  </section>
 </template>
 
 <style lang="scss">
@@ -39,18 +41,18 @@ export default {
     justify-content: space-between;
 
     .fake-text {
-        width: 300px;
-        height: 20px;
-        border-radius: 10px;
-        background-color: var(--lightGrey);
+      width: 300px;
+      height: 20px;
+      border-radius: 10px;
+      background-color: var(--lightGrey);
     }
 
     .fake-paragraph {
-        width: 400px;
-        height: 14px;
-        border-radius: 10px;
-        background-color: var(--lightGrey);
-        display: inline-block;
+      width: 400px;
+      height: 14px;
+      border-radius: 10px;
+      background-color: var(--lightGrey);
+      display: inline-block;
     }
   }
 
@@ -68,12 +70,12 @@ export default {
   .fake-img-square {
     width: 400px;
     height: 400px;
-    border-top-left-radius: 30px;
+    border-radius: 187px 0px 0px 0px;
     background-color: var(--lightGrey);
 
     i {
-        color: white;
-        font-size: 5rem;
+      color: white;
+      font-size: 5rem;
     }
   }
 }
