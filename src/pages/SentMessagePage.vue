3c8fe37240b6c0302d5b2424ed4505cb11205b71
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-02-22 14:36:55 +0100

    Fix: send messages

--- src/pages/SentMessagePage.vue
+++ src/pages/SentMessagePage.vue
@@ -1,50 +1,57 @@
 <script>
 export default {
-    name: 'SentMessagePage'
-}
+  name: "SentMessagePage",
+};
 </script>
 
 <template>
-    <div class="container-fluid d-flex justify-content-center">
-        <div class="row row-cols-1 row-cols-md-2 w-75">
-            <div class="col text-center">
-                <h2>Well done!</h2>
-                <h3>Your message was sent to our host!</h3>
+  <div class="container-fluid d-flex justify-content-center">
+    <div class="row row-cols-1 row-cols-md-2 w-75">
+      <div class="col text-center">
+        <h2>Well done!</h2>
+        <h3>Your message was sent to our host!</h3>
 
-                <router-link :to="{ name: 'home' }" class="ms-btn-filter mt-3">
-                    Back home
-                </router-link>
-            </div>
-            <div class="col text-center">
-                <img src="../assets/front-message-yes.svg" alt="sent-message" class="w-50 pt-5">
-            </div>
-        </div>
+        <a @click="this.$router.go(-1)" class="ms-btn-filter mt-3">
+          Back home
+        </a>
+      </div>
+      <div class="col text-center">
+        <img
+          src="../assets/front-message-yes.svg"
+          alt="sent-message"
+          class="w-50 pt-5"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .container-fluid {
-    background-color: var(--bgGrey);
+  background-color: var(--bgGrey);
+  height: calc(100vh - 180px);
 
-    .row {
-        background-color: white;
-        margin: 3em 0;
-        padding: 6em 2em;
+  .row {
+    background-color: white;
+    margin: 3em 0;
+    padding: 6em 2em;
+  }
+
+  h2,
+  h3 {
+    font-weight: bolder;
+    color: var(--darkBlue);
+  }
+
+  .ms-btn-filter {
+    cursor: pointer;
+    background-color: var(--darkBlue);
+    display: inline-block;
+    text-decoration: none;
+
+    &:hover {
+      color: white;
     }
-
-    h2,
-    h3 {
-        font-weight: bolder;
-        color: var(--darkBlue);
-    }
-
-    .ms-btn-filter {
-        background-color: var(--darkBlue);
-        display: inline-block;
-
-        &:hover {
-            color: white;
-        }
-    }
+  }
 }
 </style>
